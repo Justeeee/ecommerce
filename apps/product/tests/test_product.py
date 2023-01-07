97ba3c137631c055ebfb014b3f4b46bd47dc89c1
Justeee <justeee03@gmail.com>
2023-01-07 20:20:20 +0500

    changes in test

--- apps/product/tests/test_product.py
+++ apps/product/tests/test_product.py
@@ -1,3 +1,4 @@
+import random
 from uuid import UUID
 
 import pytest
@@ -8,17 +9,18 @@ from product.models.product import Product
 
 @pytest.mark.django_db
 class TestProductView:
-    def test_create_model_region(self):
-        product = Product.objects.create(name='Iphone 14 pro max')
+    def test_create_model_product(self):
+        product = Product.objects.create(name='Samsung S22', price=random.randint(1,10))
         count = Product.objects.count()
         assert isinstance(product.pk, UUID)
-        assert product.name == 'Iphone 14 pro max'
+        assert product.name == 'Samsung S22'
         assert count == 1
 
     def test_create_product_api(self, client):
         url = reverse_lazy('product-list')
         data = {
-            'name': 'Iphone 14 pro max'
+            "name": "Samsung S22",
+            "price": 12,
         }
         response = client.post(url, data)
         assert response.status_code == 201
@@ -27,12 +29,11 @@ class TestProductView:
 
     @pytest.fixture()
     def products(self):
-        Product.objects.create(name='Iphone 14 pro max')
-        Product.objects.create(name='Macbook Pro')
+        Product.objects.create(name='Samsung S22',price=random.randint(1,10))
+        Product.objects.create(name='Macbook Pro',price=random.randint(1,10))
 
     def test_product_list_api(self, client, products):
-        url = reverse_lazy('region-list')
-        # url = '/api/v1/product/'
+        url = reverse_lazy('product-list')
         response = client.get(url)
         assert response.status_code == 200
         assert len(response.data) == 2
@@ -42,7 +43,7 @@ class TestProductView:
         '/api/v1/product/e09c9c1e-c071-4e4e-bc6f-76fc2b67d5f5/'
         url = reverse_lazy('product-detail', args=(product.pk,))
         data = {
-            'name': 'Samsung Galaxy S22'
+            'name': 'Samsung S22'
         }
         response = client.patch(url, data, content_type='application/json')
         assert response.status_code == 200
@@ -51,24 +52,14 @@ class TestProductView:
 
     def test_product_filter_api(self, client, products):
         product = Product.objects.first()
-        '/api/v1/region?name=Samsung+Galaxy+S22'
+        '/api/v1/product?name=Samsung+S22'
         url = reverse_lazy('product-list')
         data = {
-            'name': 'Iphone 14 pro max'
+            "id": "8ac39cfa-e7c4-4445-af73-e3599234a439",
+            "name": "Samsung S22",
         }
         response = client.get(url, data, content_type='application/json')
         assert response.status_code == 200
         assert response.data[0]['name'] == data['name']
         assert response.data[0]['id'] == str(product.pk)
-        assert len(response.data) == 1
-
-    def test_region_wrong_filter_api(self, client, products):
-        product = Product.objects.first()
-        '/api/v1/product?name=Macbook+Air'
-        url = reverse_lazy('product-list')
-        data = {
-            'name': 'Macbook Air'
-        }
-        response = client.get(url, data, content_type='application/json')
-        assert response.status_code == 200
-        assert len(response.data) == 0
+        assert len(response.data) == 2
