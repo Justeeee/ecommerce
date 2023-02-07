beec1bb4cfc8a75d240a6da18251882e1cd1dcc5
Justeee <justeee03@gmail.com>
2023-02-07 23:55:37 +0500

    changes

--- apps/product/models/__init__.py
+++ apps/product/models/__init__.py
@@ -1,4 +1,4 @@
 from apps.product.models.product import Product
 from apps.product.models.shop import Shop
-from apps.product.models.category import Category
+from apps.product.models.category import Category, SubCategory
 
